9c2163b1fccca592e1d3203c1a703f39db35c599
Muhammad Nasrullah <muhammad.nasrullah@aquila360.com>
2025-01-28 10:27:10 +0500

    restore the prompt change

--- src/ai.tsx
+++ src/ai.tsx
@@ -1,14 +1,13 @@
 import { HfInference } from "@huggingface/inference"
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. Moreover if the recipe is other than Pakistani recipe, mention the recipe origin country as well
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.
 `
 
 const hf = new HfInference(import.meta.env.VITE_HF_TOKEN)
 
 export async function getRecipeFromMistral(ingredientsArr: any) {
   const ingredientsString = ingredientsArr.join(", ")
-  console.log("ingredientsString", ingredientsString)
   try {
     const response = await hf.chatCompletion({
       model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
@@ -21,7 +20,7 @@ export async function getRecipeFromMistral(ingredientsArr: any) {
       ],
       max_tokens: 1024,
     })
-    console.log("ingredientsString", ingredientsString)
+
     return response.choices[0].message.content
   } catch (err: any) {
     console.error(err.message)
