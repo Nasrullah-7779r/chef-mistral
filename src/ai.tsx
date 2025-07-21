e49ae0822607b0a113d386d11f1083cd16fc8ab2
Muhammad Nasrullah <muhammad.nasrullah@aquila360.com>
2025-07-21 22:44:46 +0500

    refactor: update recipe fetching logic to use new API structure and simplify request format

--- src/ai.tsx
+++ src/ai.tsx
@@ -1,35 +1,29 @@
-const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.
-`
+export async function getRecipeFromMistral(ingredientsArr: string[]) {
+  if (ingredientsArr.length === 0) {
+    return ""
+  }
 
-export async function getRecipeFromMistral(ingredientsArr: any) {
-  const ingredientsString = ingredientsArr.join(", ")
   try {
     const response = await fetch(
-      "https://api.together.xyz/v1/chat/completions",
+      import.meta.env.VITE_API_BASE_URL_LOCAL + "recipe",
       {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_TOGETHER_AI_API_KEY}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
-          messages: [
-            { role: "system", content: SYSTEM_PROMPT },
-            {
-              role: "user",
-              content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
-            },
-          ],
-          max_tokens: 512,
+          ingredients: ingredientsArr
+            .map((ingredient) => ingredient.trim())
+            .filter(Boolean),
         }),
       }
     )
-
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
     const data = await response.json()
 
-    return data.choices[0].message.content
+    return data.recipe
   } catch (err: any) {
     console.error(err.message)
   }
